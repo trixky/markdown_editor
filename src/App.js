4476ffc50585cbc47850967d76c72731d04aa9b1
trixky <mathisbois.dev@gmail.com>
2020-06-10 18:54:22 +0200

    add DOMpurify

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { sampleText } from './sampleText'
+import marked from 'marked'
+import DOMpurify from 'dompurify'
 
 class App extends Component {
+	state = {
+		text: sampleText
+	}
+
+	componentDidMount () {
+		console.log('je suis monte');
+	}
+
+	componentDidUpdate () {
+		console.log('je suis update')
+	}
+
+	handleChange = event => {
+		const text = event.target.value
+		this.setState({ text })
+	}
+
+	renderText = text => {
+		const __html = marked(DOMpurify.sanitize(text), {})
+		return {__html};
+	}
+
 	render() {
 		return (
 			<div className='container'>
 				<div className='row'>
 					<div className='col-sm-6'>
 						<textarea
+							onChange={this.handleChange}
+							value={this.state.text}
 							className='form-control'
-							row='35'>
+							rows='35'>
 						</textarea>
 					</div>
 					<div className='col-sm-6'>
-						Resultat
+						<div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
 					</div>
 				</div>
 			</div>
